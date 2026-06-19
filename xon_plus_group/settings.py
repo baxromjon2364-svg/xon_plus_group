f598bdf928dd2b83d237c519503bb2014aa71713
Ahrorbek <baxromjon2364@gmail.com>
2026-06-19 14:24:20 +0500

    databasega ozgarish

--- xon_plus_group/settings.py
+++ xon_plus_group/settings.py
@@ -1,3 +1,5 @@
+import os
+import dj_database_url
 """
 Django settings for xon_plus_group project.
 
@@ -75,10 +77,11 @@ WSGI_APPLICATION = 'xon_plus_group.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
